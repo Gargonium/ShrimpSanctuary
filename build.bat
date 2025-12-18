3f97b2745d7800af700d6d4b5e67588d3dde1d58
Gargonium <91950247+Gargonium@users.noreply.github.com>
2025-12-18 07:06:53 +0700

    Remove another platforms from build.bat ('cause they don`t work...)

--- build.bat
+++ build.bat
@@ -1,55 +1,15 @@
 @echo off
 setlocal enabledelayedexpansion
 
-REM Выбор архитектуры
-echo Choose architecture:
-echo 1. Windows 64-bit (default)
-echo 2. Windows 32-bit
-echo 3. Linux 64-bit
-echo 4. macOS 64-bit
-set /p choice="Enter the number [1]: "
-
-if "%choice%"=="" set choice=1
-
-
-
-if "%choice%"=="2" (
-    set ARCH=386
-    set SUFFIX=_32bit.exe
-    set OS=windows
-) else if "%choice%"=="3" (
-    set ARCH=amd64
-    set SUFFIX=_linux
-    set OS=linux
-) else if "%choice%"=="4" (
-    set ARCH=amd64
-    set SUFFIX=_macos
-    set OS=darwin
-) else (
-    set ARCH=amd64
-    set SUFFIX=.exe
-    set OS=windows
-)
-
 REM Создаем папку для релиза с указанием архитектуры
-set RELEASE_FOLDER=release_%OS%_%ARCH%
+set RELEASE_FOLDER=release_windows_amd64
 if not exist "%RELEASE_FOLDER%" mkdir "%RELEASE_FOLDER%"
 
 REM Сборка для выбранной платформы
-echo Start build for %OS% %ARCH%...
-set GOOS=%OS%
-set GOARCH=%ARCH%
-if "%OS%"=="windows" (
-    go build -ldflags="-H windowsgui -s -w" -o "%RELEASE_FOLDER%\ShrimpSanctuary%SUFFIX%" ./cmd
-)  else (
-    go build -ldflags="-s -w" -o "%RELEASE_FOLDER%\ShrimpSanctuary%SUFFIX%" ./cmd
-)
+echo Start build for windows amd64...
+set GOOS=windows
+set GOARCH=amd64
 
+go build -ldflags="-H windowsgui -s -w" -o "%RELEASE_FOLDER%\ShrimpSanctuary.exe" ./cmd
 
-cd "%RELEASE_FOLDER%"
-powershell "Compress-Archive -Path * -DestinationPath 'ShrimpSanctuary_%OS%_%ARCH%.zip' -Force"
-cd ..
-
-echo.
-echo Archive created: %RELEASE_FOLDER%\ShrimpSanctuary_%OS%_%ARCH%.zip
 pause
